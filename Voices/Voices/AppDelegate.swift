4b85742d89d13b61a9acba5ccd3313f758181440
Mazyad Alabduljaleel <mazjaleel@gmail.com>
2014-11-08 22:43:06 +0400

    [NEW]: create initial swift project

---
+++ Voices/Voices/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Voices
+//
+//  Created by Mazyad Alabduljaleel on 11/1/14.
+//  Copyright (c) 2014 Assembly. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Voices/Voices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Voices
+//
+//  Created by Mazyad Alabduljaleel on 11/1/14.
+//  Copyright (c) 2014 Assembly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
