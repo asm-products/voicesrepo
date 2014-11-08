fc7e8295b57e1ba0bf0c5769e157b3b38e9bd090
Mazyad Alabduljaleel <mazjaleel@gmail.com>
2014-11-09 00:15:11 +0400

    [NEW]: initial implementation of content view controllers

---
+++ Voices/Voices/App/ContentViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ContentViewController.swift
+//  Voices
+//
+//  Created by Mazyad Alabduljaleel on 11/8/14.
+//  Copyright (c) 2014 Assembly. All rights reserved.
+//
+
+import UIKit
+
+/** Abstract superclass to the view controllers added to
+ *  the content view controller
+ */
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview({
+            let label = UILabel()
+            label.text = NSStringFromClass(self.dynamicType.self)
+            label.sizeToFit()
+            return label
+        }())
+    }
+
+}
--- Voices/Voices/RootViewController.swift
+++ Voices/Voices/RootViewController.swift
@@ -8,14 +8,62 @@
 
 import UIKit
 
+/** The root view controller contains the whole app, and manages the
+ *  content scroll view which contains the content view controllers
+ */
 class RootViewController: UIViewController {
+    
+    private let contentViewControllers: [UIViewController]
+    
+    
+    @IBOutlet var contentScrollView: UIScrollView?
 
+    
+    required init(coder aDecoder: NSCoder) {
+        let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        contentViewControllers = [
+            "ContactsViewController",
+            "RecordViewController",
+            "InboxViewController"
+        ].map {
+            mainStoryboard.instantiateViewControllerWithIdentifier($0) as UIViewController
+        }
+        
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Keep calm, and assert all your assumptions
+        assert(contentScrollView != nil, "Content scroll view must be connected in IB")
+        let scrollView = contentScrollView!
+        scrollView.contentSize.width = CGFloat(contentViewControllers.count) * self.view.bounds.width
+        scrollView.contentOffset.x = self.view.bounds.width
+        
+        var xOffset = CGFloat(0)
+        for viewController in contentViewControllers {
+            viewController.view.frame.origin.x = xOffset
+            viewController.view.frame.size = scrollView.bounds.size
+            scrollView.addSubview(viewController.view)
+            
+            xOffset += self.view.bounds.width
+        }
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+    
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
     }
 
 }
 
+/** Subclass the Root view to draw the background gradient
+ */
 class RootView: UIView {
     
     override class func layerClass() -> AnyClass {
